b58f9ca295c5d26b6db06cc64ee1b48f6f29c9c4
testing <test@123.com>
2017-09-24 17:02:29 +0400

    Add POST/users/login

--- server/server.js
+++ server/server.js
@@ -111,8 +111,6 @@ app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
-
-
   user.save().then((user) => {
     return user.generateAuthToken();
   }).then((token) => {
@@ -123,10 +121,21 @@ app.post('/users', (req, res) => {
 });
 
 
-
-
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+
+app.post('/users/login', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+   return user.generateAuthToken().then((token) => {
+     res.header('x-auth',token).send(user);
+   });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
